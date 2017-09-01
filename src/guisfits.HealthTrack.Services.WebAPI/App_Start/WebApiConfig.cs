09d0fd142bb94b42124c50ac353dda602795bacd
Guilherme <guisfits@hotmail.com>
2017-09-01 16:47:49 -0300

    Cors

--- src/guisfits.HealthTrack.Services.WebAPI/App_Start/WebApiConfig.cs
+++ src/guisfits.HealthTrack.Services.WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace guisfits.HealthTrack.Services.WebAPI
 {
@@ -10,7 +11,8 @@ namespace guisfits.HealthTrack.Services.WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            var cors = new EnableCorsAttribute("*", "*", "GET,POST");
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
