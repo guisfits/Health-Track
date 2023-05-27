1422ea816b38867fb67f97133bbe766a2076cb3d
Guilherme Camargo <guisfits@hotmail.com>
2023-05-26 22:50:28 -0300

    removing www of the project

--- HealthTrack.MVC/www/scripts/efeitos-logo.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $("#logotipo").mouseover(function() {
-        $("#logotipo").animate({ width: "260", height: "200" });
-    });
-    $("#logotipo").mouseout(function() {
-        $("#logotipo").animate({ width: "230", height: "170" });
-    });
-});
